410a357a8d0e26c6446c57ec78f04e889104405c
akasharora963 <akasharora963@gmail.com>
2025-03-24 20:42:29 +0530

    remove: console logs

--- src/services/BatchService.ts
+++ src/services/BatchService.ts
@@ -71,7 +71,6 @@ export class BatchService {
     ): Promise<boolean> {
         const erc20Contract = await this.getERC20Contract(tokenAddress);
         const currentAllowance = await erc20Contract.allowance(owner, spender);
-        console.log("currentAllowance", currentAllowance)
         return BigInt(currentAllowance) > amount;
     }
 
@@ -119,8 +118,6 @@ export class BatchService {
             );
             const finalGasPrice = gasPrice > optimalGasPrice ? gasPrice : optimalGasPrice;
 
-            console.log("Final Gas Price:", finalGasPrice);
-
             // Use the full contract interface to encode the function call for aggregate3.
             const iface = new ethers.Interface(MULTICALL3_ABI);
             const calls = transactionData.to.map((target, i) => ({
@@ -130,9 +127,6 @@ export class BatchService {
                 callData: transactionData.data[i],
             }));
             const encodedData = iface.encodeFunctionData("aggregate3Value", [calls]);
-
-            console.log("Encoded Data:", encodedData);
-            console.log("Multicall Address:", this.config.multicallAddress);
 
             // Get signer and sender address.
             const signer = this.provider.getSigner();
@@ -146,9 +140,8 @@ export class BatchService {
 
             // Calculate total ETH value to send
             const totalValue = transactionData.values.reduce((acc, val) => acc + val, BigInt(0));
-            console.log("Total ETH value to send:", totalValue.toString());
 
-            // Estimate gas using the underlying JSON-RPC provider.
+            // Estimate gas using the underlying RPC provider.
             const gasLimit = await this.provider.getProvider().estimateGas({
                 from: senderAddress,
                 to: this.config.multicallAddress,
@@ -240,9 +233,7 @@ export class BatchService {
         }
 
         const batchTxnParams = await this.prepareBatchTransaction(ethBatch, erc20Batch);
-        console.log("batchTxnParams", batchTxnParams);
         const gasLimit = await this.estimateGas(batchTxnParams, gasPrice);
-        console.log("Gas Limit:", gasLimit);
 
         return await this.handleSendTransaction(batchTxnParams, gasLimit, gasPrice);
     }
@@ -341,14 +332,10 @@ export class BatchService {
             callData: "0x",
         }));
 
-        console.log("Calls:", calls);
-
         try {
-            // Use the full contract interface to encode the aggregate3 function call.
             const iface = new ethers.Interface(MULTICALL3_ABI);
 
             const encodedData = iface.encodeFunctionData("aggregate3Value", [calls]);
-            console.log("Encoded Data:", encodedData);
 
             return {
                 data: encodedData || "0x",
@@ -424,12 +411,9 @@ export class BatchService {
                 };
             });
 
-            console.log("ERC20 Calls:", calls);
 
-            // Use the full multicall interface to encode the aggregate3Value function call.
             const iface = new ethers.Interface(MULTICALL3_ABI);
             const encodedData = iface.encodeFunctionData("aggregate3Value", [calls]);
-            console.log("Encoded ERC20 Data:", encodedData);
 
             return {
                 data: encodedData || "0x",
